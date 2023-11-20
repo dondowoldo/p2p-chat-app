775e957f9e62754c44be1b4bf123584579563fe1
Daniel Valasek <daniel.valasek@yahoo.com>
2023-11-20 16:33:22 +0100

    added DTOvaliation

--- src/main/java/com/greenfoxacademy/p2pchat/controllers/AppRestController.java
+++ src/main/java/com/greenfoxacademy/p2pchat/controllers/AppRestController.java
@@ -1,9 +1,12 @@
 package com.greenfoxacademy.p2pchat.controllers;
 
+import com.greenfoxacademy.p2pchat.dtos.ErrorDTO;
 import com.greenfoxacademy.p2pchat.dtos.MessageDTO;
 import com.greenfoxacademy.p2pchat.services.MessageService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,12 +18,15 @@ public class AppRestController {
     }
 
     @PostMapping("/api/message/receive")
-    public ResponseEntity<Object> receiveMsg(MessageDTO messageDTO) {
-        if (messageService.existsById(messageDTO.message().getId())) {
-//            nasrat
+    public ResponseEntity<Object> receiveMsg(@RequestBody MessageDTO messageDTO) {
+        String message = messageService.validateDTO(messageDTO);
+        if (message != null) {
+            return ResponseEntity.status(401).body(new ErrorDTO("error", message));
         }
-        messageService.saveMessage(messageDTO.message());
-        return ResponseEntity.status(201).build();
-
+        if (!messageService.existsById(messageDTO.message().getId())) {
+            messageService.saveMessage(messageDTO.message());
+            return ResponseEntity.status(201).build();
+        }
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/greenfoxacademy/p2pchat/services/MessageService.java
+++ src/main/java/com/greenfoxacademy/p2pchat/services/MessageService.java
@@ -1,11 +1,14 @@
 package com.greenfoxacademy.p2pchat.services;
 
+import com.greenfoxacademy.p2pchat.dtos.MessageDTO;
 import com.greenfoxacademy.p2pchat.models.Message;
 import com.greenfoxacademy.p2pchat.repositories.MessageRepo;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class MessageService {
@@ -25,6 +28,31 @@ public class MessageService {
 
     public boolean existsById(UUID uuid) {
         return messageRepo.existsById(uuid);
+    }
+
+    public String validateDTO(MessageDTO messageDTO) {
+
+        List<String> messages = new ArrayList<>();
+
+        if (messageDTO.message().getId() == null) {
+            messages.add("message.id");
+        }
+        if (messageDTO.message().getUsername() == null || messageDTO.message().getUsername().isEmpty()) {
+            messages.add("message.username");
+        }
+        if (messageDTO.message().getText() == null || messageDTO.message().getText().isEmpty()) {
+            messages.add("message.text");
+        }
+        if (messageDTO.message().getTimestamp() == null) {
+            messages.add("message.timestamp");
+        }
+        if (messageDTO.client().id() == null || messageDTO.client().id().isEmpty()) {
+            messages.add("client.id");
+        }
+        if (messages.isEmpty()) {
+            return null;
+        }
+        return "Missing field(s): " + String.join(", ", messages);
     }
 
 
